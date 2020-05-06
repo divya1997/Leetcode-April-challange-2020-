60cf5cd7c4d10c577fb23ebf4cc499d4f3006b57
divya1997 <divyamehta199707@gmail.com>
2020-05-06 18:38:59 +0530

    Update SingleNumber.cpp

--- Week1/SingleNumber.cpp
+++ Week1/SingleNumber.cpp
@@ -1,1 +1,9 @@
+//Method 1(Using Xor)
+int singleNumber(vector<int>& nums) {
+    int number = nums[0];
+    for(int i =1;i<nums.size();i++){
+        number = number^nums[i];
+    }
+    return(number);
+}
 
