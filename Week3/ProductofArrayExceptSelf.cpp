de203c123de5acf7e1552dd6ad71be35e30c4149
divya1997 <divyamehta199707@gmail.com>
2020-05-09 10:16:45 +0530

    Update ProductofArrayExceptSelf.cpp

--- Week3/ProductofArrayExceptSelf.cpp
+++ Week3/ProductofArrayExceptSelf.cpp
@@ -1,1 +1,19 @@
+//Product of Array Except Self
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans;
+        ans.push_back(1);
+        for(int i=1;i<nums.size();i++){
+            ans.push_back(nums[i-1]*ans[i-1]);
+        }
+        int Rprod = 1;
+        for(int j=nums.size()-1;j>=0;j--){
+            ans[j] = Rprod*ans[j];
+            Rprod = Rprod*nums[j];
+        }
+        return(ans);
+        
+    }
+};
 
